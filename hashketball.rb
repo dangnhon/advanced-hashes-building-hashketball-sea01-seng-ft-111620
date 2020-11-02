069848792b441e7cc7a90f2eb1b71bedff572021
Nhon Dang <dang.nhon12345@yahoo.com>
2020-11-02 22:10:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -87,7 +87,7 @@ def game_hash
         :rebounds => 12,
         :assists => 12,
         :steals => 4,
-        :blocks => 5, 
+        :blocks => 5,
         :slam_dunks => 5},
         {
         :player_name => "Ben Gordon",
